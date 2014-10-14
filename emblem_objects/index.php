94a7cf7e030546d9d0860e6168439d801d55f633
alexadorsey <alexadorsey@hotmail.com>
2014-10-14 15:53:06 -0700

    Added the search box
    Changed index.html to index.php, so you’ll need to view it on a server
    instead of just locally.
    I made the search icon a search submit button, and thus moved it to the
    right because that’s usually where a submit button goes. However, it
    can be easily moved to the left.

--- emblem_objects/index.php
+++ emblem_objects/index.php
@@ -3,17 +3,26 @@
 <html lang="en">
 <head>
     <title>Emblem Objects</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <script>
         /*
         </body>
         </html>
         */
     </script>
+    <script language="javascript">
+        function send() {
+          //  document.theform.submit();
+            console.log("Sending form");
+        }
+</script>
     <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="framework.css">
     
 </head>
+
+
 <body>
     
     <div id="wrapper">
@@ -36,7 +45,17 @@
      
     <!-- NAVIGATION -->  
     <div id="navigation">
-        <div class="container">   
+        <div class="container">
+            
+           
+            
+            <form name="search-field" id="search-field" action="" method=POST>
+                <input name="search-input" />
+                <button name="search-submit" type="submit"><img src="images/icon_search.png" alt="Search" height="15px" width="15px"/></button>
+            </form>
+            
+
+            
         </div>         
     </div>
     <div style="clear:both"></div>
@@ -55,17 +74,30 @@
     <!-- FOOTER -->  
     <div id="footer">
         <div id="icons-container">
-            <img class="social-icon" src="images/icon_twitter.png"/>
-            <img class="social-icon" src="images/icon_tumblr.png"/>
-            <img class="social-icon" src="images/icon_youtube.png"/>
-            <img class="social-icon" src="images/icon_google+.png"/>
-            <img class="social-icon" src="images/icon_facebook.png"/>
+            <a href="#"><img class="social-icon" src="images/icon_twitter.png"/></a>
+            <a href="#"><img class="social-icon" src="images/icon_tumblr.png"/></a>
+            <a href="#"><img class="social-icon" src="images/icon_youtube.png"/></a>
+            <a href="#"><img class="social-icon" src="images/icon_google+.png"/></a>
+            <a href="#"><img class="social-icon" src="images/icon_facebook.png"/></a>
         </div>
     </div>
     <div style="clear:both"></div>
     </div>
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="home.js"></script> 
-    <script src="framework.js"></script> 
+    <script src="framework.js"></script>
+    
+    
+    
+    <!-- PHP -->
+    <?php
+    
+        // Submit the search
+        if(isset($_POST['search-submit'])){  //check if form was submitted
+            $input = $_POST['search-input']; //get input text
+            echo '<script type="text/javascript">console.log("'.$input.'");</script>';  // log input to the console
+        }
+        
+    ?>
 </body>
 </html>
