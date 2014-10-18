47e5e698233d8ac08b3e41e63a0bfd9f50386af7
Dan Chen <dyc5828@gmail.com>
2014-10-18 01:57:05 -0700

    Updated json-store-objects.php
    Changed array to associative
    Reduced code
    JS namespace var

--- php/json-store-objects.php
+++ php/json-store-objects.php
@@ -6,28 +6,31 @@
  * Time: 6:48 PM
  */
 include_once 'db-connect-con.php';
-//sql statement to select (maybe do a view?
+//sql statement to select (maybe do a view?)
 $sql = "SELECT item_id, item_tag, subcategory_desc, item_name, user_first_name, user_last_name, item_description, category_desc, item_type, collection_desc
         FROM item, item_category, item_subcategory, user_table, collection
         WHERE item.subcategory_id = item_subcategory.subcategory_id AND item_subcategory.category_id = item_category.category_id AND item.item_designer = user_table.user_ID AND item.collection_id = collection.collection_id";
-$r = mysqli_query($con, $sql);
-if (!$r){
+$items_results = mysqli_query($con, $sql);
+
+$items_array = array();
+
+if (!$items_results){
     echo mysqli_error($con);
 }
 else {
     $i = 0; // This keeps the first one separate from the rest
-    while ($row = mysqli_fetch_array($r)){
-        if ($i == 0){//if first one
-            $object = array(array($row['item_id'], $row['item_tag'], $row['subcategory_desc'], $row['item_name'], $row['user_first_name'], $row['user_last_name'], $row['item_description'], $row['category_desc'], $row['item_type'], $row['collection_desc']));
-            $i++; //no longer the "first" one
-        }
-        else { //pushing another array to the second dimension
-            array_push($object, array($row['item_id'], $row['item_tag'], $row['subcategory_desc'], $row['item_name'], $row['user_first_name'], $row['user_last_name'], $row['item_description'], $row['category_desc'], $row['item_type'], $row['collection_desc']));
-        }
-  }
+    while ($row = mysqli_fetch_array($items_results)) {
+        //pushing another array to the second dimension
+        array_push($items_array, array('id'=>$row['item_id'], 'tag'=>$row['item_tag'], 'subcategory'=>$row['subcategory_desc'], 'name'=>$row['item_name'], 'designer_first_name'=>$row['user_first_name'], 'designer_last_name'=>$row['user_last_name'], 'description'=>$row['item_description'], 'category'=>$row['category_desc'], 'type'=>$row['item_type'], 'collection_name'=>$row['collection_desc']));
+    }
 }
+
+$items_json = json_encode($items_array);
+
 ?>
 <script>
-    var object = <?php echo json_encode($object, JSON_FORCE_OBJECT); ?>; // this creates the JSON
-    console.log(object); //temporary, to look at the object
+    var store = window.store || {};
+
+    store.items = <?php echo $items_json; ?>; // this creates the JSON
+    console.log(store.items); //temporary, to look at the object
 </script>
