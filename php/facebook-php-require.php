e77eda95dcb021fe4d896bf14aeda974bce070e4
Dan Chen <dyc5828@gmail.com>
2014-10-20 15:00:04 -0700

    FB PHP

--- php/facebook-php-require.php
+++ php/facebook-php-require.php
@@ -1,5 +1,17 @@
 <?php
 // Facebook PHP Require
 
-define('facebook_php_sdk_v4_src', 'facebook-php-sdk/src');
-require '/facebook-php-sdk/autoload.php';
+// define('FACEBOOK_SDK_V4_SRC_DIR', __DIR__.'/facebook-php-sdk/src');
+// require __DIR__.'/facebook-php-sdk/autoload.php';
+
+require_once (__DIR__.'/facebook-php-sdk/Facebook/FacebookSession.php');
+require_once (__DIR__.'/facebook-php-sdk/Facebook/FacebookJavaScriptLoginHelper.php');
+require_once (__DIR__.'/facebook-php-sdk/Facebook/FacebookSignedRequestFromInputHelper.php');
+
+// use Facebook\FacebookSession;
+// use Facebook\FacebookJavaScriptLoginHelper;
+// use Facebook\FacebookSignedRequestFromInputHelper;
+
+// include __DIR__.'/facebook-php-sdk/src/test.php';
+
+// FacebookSession::setDefaultApplication('360442404114516','3685eca8c02b480bb2027336c8b820e3');
--- sandbox/dy/facebook_test.php
+++ sandbox/dy/facebook_test.php
@@ -1,3 +1,9 @@
+<head>
+	<title></title>
+	<script type="text/javascript" src="../../js/facebook-js-sdk/facebook-sdk.js"></script>
+	<script type="text/javascript" src="../../js/facebook-js-sdk/facebook-api.js"></script>
+
+</head>
 <?php
 // Facebook Php Sdk Test
 // var_dump(include_once '../../php/facebook-php-require.php');
@@ -5,15 +11,64 @@ include_once '../../php/facebook-php-require.php';
 
 echo '<hr/>Hello!';
 
-use Facebook\FacebookSession;
+FacebookSession::setDefaultApplication('360442404114516','3685eca8c02b480bb2027336c8b820e3');
 
-FacebookSession::setDefaultApplication('','');
-$session = new FacebookSession('')
+/* Get Auth from another means
+ */
+//$session = new FacebookSession('')
 
 // If facebook session true
-if ($session) {
+//if ($session) {
 	// query db for username
 	// if usernot found, create new user with facebook info
 	
 	// create php session
+//}
+
+/* Get Auth from JS
+ */
+$helper = new FacebookJavaScriptLoginHelper();
+
+try {
+	$session = $helper->getSession();
+} catch (FacebookRequestException $ex) {
+	echo "<br/>Facebook Returned Error";
+} catch (\Exception $ex) {
+	echo "<br/>Validation Failed / Local Issues";
 }
+
+if ($session) {
+	echo "<hr/>starting session...";
+}
+
+?>
+
+<boody>
+<script>
+
+facebook.init({
+  appID: '609528499167439',
+  msg: 'Passed',
+});
+
+</script>
+
+<div
+  class="fb-like"
+  data-href="http://www.google.com"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
+<!-- data-href="http//:localhost:8080/facebook/facebook-login.html" -->
+
+<fb:login-button 
+	scope="public_profile,email"
+	autologoutlink="true"
+	onlogin="checkLoginState();">
+</fb:login-button>
+
+<div id="status">
+</div>
+
+</body>
