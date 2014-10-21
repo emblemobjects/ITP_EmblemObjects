96c643865064864b5ebd5c26c824db05a132c326
Kevin Wang <kevinjwa@usc.edu>
2014-10-21 01:16:03 -0700

    added limtiing factors for file upload

--- sandbox/kevin_sandbox/upload.php
+++ sandbox/kevin_sandbox/upload.php
@@ -5,13 +5,24 @@ $target_dir = $target_dir . basename ( $_FILES["uploadFile"]["name"]);
 $uploadOk = 1;
 
 //insert file constraints (size, types, etc)
-
-if(move_uploaded_file($_FILES["uploadFile"]["tmp_name"],$target_dir))
-{//moves the file where it should be going
-    echo "The file " . basename($_FILES["uploadFile"]["name"]). has been uploaded ";"
+if($uploadFile_size > 500000)
+{
+    echo "File is too large "
+    $uploadOk = 0;
 }
-else{
-    echo "Sorry, there was an error uploading your file.";
+if(!($uploadFile_type == "image/gif")){
+    echo "Sorry, only ___ types of files are allowed";
+    $uploadOk = 0;
+}
+if($ok ==0) {
+    echo "Your file failed to upload ";
+}
+else {
+    if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_dir)) {//moves the file where it should be going
+        echo "The file " . basename($_FILES["uploadFile"]["name"]) . has been uploaded ";"
+} else {
+        echo "Sorry, there was an error uploading your file.";
+    }
 }
 
 ?>
