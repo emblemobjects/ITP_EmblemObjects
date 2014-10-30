76a5c665be05619254054a97c3a2047d8770a3fb
ziweiliu <ziweiliu@usc.edu>
2014-10-29 21:19:55 -0700

    Fixed some merge issues.... should work now

--- customize/index.php
+++ customize/index.php
@@ -2,19 +2,16 @@
 session_start();
 include "customize-session.php";
 if (empty($_REQUEST['item_id'])) {
-    //header("location: ../home/index.php");
-    //exit();
+    header("location: ../home/index.php");
 }
 include_once '../php/config.php';
 include_once '../php/helper.php';
 include_once '../php/json-store-objects.php';
 include_once '../php/items.php';
 
-//$item_id = $_REQUEST['item_id'];
-$item_id = "1";
+$item_id = $_REQUEST['item_id'];
 $item_name = items::get_field_value($item_id, "name");
-//$detail_id = $_REQUEST['detail_id'];
-$detail_id = "2";
+$detail_id = $_REQUEST['detail_id'];
 $material_name = items::get_detail_info($item_id, $detail_id, "material");
 $material_id = items::get_detail_info($item_id, $detail_id, "material_id");
 $price = items::get_detail_info($item_id, $detail_id, "price");
@@ -61,7 +58,7 @@ $size = items::get_detail_info($item_id, $detail_id, "size");
                     <div class="fileUpload">
                         <input type="hidden" name="newFileName" value="customer_artwork"/>
                         <div id="uploadFile"><img id = "previewObject" src="<?php echo items::getPrimaryImage($GLOBALS['items_array'], $item_id); ?>" /></div>
-                        <input class="upload" type="file" id="uploadButton" name="uploadButton" accept=".gif, .png, .jpg, .jpeg, .ai">
+                        <input type="file" id="uploadButton" name="uploadButton" accept=".gif, .png, .jpg, .jpeg, .ai" />
                         <span id="upload-error"><?php echo $errorMessage ?></span><br>
                     </div>
 
@@ -71,14 +68,14 @@ $size = items::get_detail_info($item_id, $detail_id, "size");
                     <input type="hidden" name="item_id" value="<?php echo $item_id ?>" />
                     <input type="hidden" name="material_id" value="<?php echo $material_id ?>" />
                     <br/>
-                    <div class="readOnly">Material: <input class = "readOnlyBox" type="text" size='25' name="material_name" value="<?php echo $material_name ?>" readonly /></div>
-                    <div class="readOnly">Size: <input class = "readOnlyBox" type="text" size='25' name="size" value="<?php echo $size; ?>" readonly /></div>
-                    <div class="readOnly">Price: <input class = "readOnlyBox" type="text" size='25' name="price" value="<?php echo $price ; ?>" readonly /></div>
-                    <br/><br/>
-                    <div>First Name:</div><input type="text" name="firstName" size='25' value="<?php echo $firstName ?>" required>
-                    <div>Last Name:</div><input type="text" name="lastName" size='25' value="<?php echo $lastName ?>" required>
-                    <div>Email:</div><input type="text" name="email" size='25' value="<?php echo $email ?>"required>
-                    <div>Message to Designer:</div><textarea name="message" rows="5" cols="27" maxlength="1000"><?php echo $message ?></textarea>
+                    <br />
+                    <div class="input">Material:</div><input class = "readOnly" type="text" size='25' name="material_name" value="<?php echo $material_name ?>" readonly /><br />
+                    <div class="input">Size:</div><input class = "readOnly" type="text" size='25' name="size" value="<?php echo $size; ?>" readonly /><br />
+                    <div class="input">Price:</div><input class = "readOnly" type="text" size='25' name="price" value="<?php echo $price ; ?>" readonly /><br />
+                    <div class="input">First Name:</div><input type="text" name="firstName" size='25' required><br />
+                    <div class="input">Last Name:</div><input type="text" name="lastName" size='25' required><br />
+                    <div class="input">Email:</div><input type="text" name="email" size='25' required><br />
+                    <div class="input">Message to Designer:</div><textarea name="message" rows="5" cols="27" maxlength="1000"></textarea><br />
                 </div>
 
                 <br style="clear:both">
