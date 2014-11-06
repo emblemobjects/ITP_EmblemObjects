f9b1fa075833ef615af5ed1c0b8bef8eebf2c946
Dan Chen <dyc5828@gmail.com>
2014-11-06 10:07:15 -0800

    Updates

--- html-template.php
+++ html-template.php
@@ -1,29 +1,37 @@
-<?php // NONE DISPLAY php includes
+<?php // NO SHOW php includes - check file path!
     include_once '/php/config.php'; // MUST config.php! check file path
+    include_once '/php/session.php'; // MUST for logged in pages
+    include_once '/php/helper.php'; // good to have
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php// includes to load on DOM ?>
-    <title>Emblem Objects</title>
+    <title>Emblem Objects Page Title</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     
     <!--External Scripts-->
+    <!--REQUIRED SCRIPTS-->
+    <script src="<?php echo DIR; ?>/js/config.js"></script>
     <script src="<?php echo $jQuery; ?>"></script>
-    <script src="../js/handlebars-v2.0.0.js"></script>
+    <script src="<?php echo DIR; ?>/js/handlebars-v2.0.0.js"></script>
+    <!-- Page Specfic Scripts-->
 
     <!--External CSS-->
+    <!--REQUIRED CSS-->
     <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="../css/core.css">
     <link rel="stylesheet" type="text/css" href="../css/header.css">
-    <link rel="stylesheet" type="text/css" href="../css/navigation.css">
-    <link rel="stylesheet" type="text/css" href="../css/body.css">
-    <link rel="stylesheet" type="text/css" href="../css/content.css">
+    <link rel="stylesheet" type="text/css" href="../css/nav.css">
     <link rel="stylesheet" type="text/css" href="../css/footer.css">
+    <!--Page Specific CSS-->
     <link rel="stylesheet" type="text/css" href="../css/home.css">
     <link rel="stylesheet" type="text/css" href="../css/items-grid.css">
+    <link rel="stylesheet" type="text/css" href="../css/content.css">
 
 </head>
 <body>
+<div id="fb-root"></div><!--Facebook Required DO NOT TOUCH-->
 
 <!-- <div id="overlay"></div>-->
 
