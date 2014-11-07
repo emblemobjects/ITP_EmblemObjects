365a1738719c4e06b7745216b93d7913f8754a59
Jose Valderruten <Josecamilo@me.com>
2014-11-06 17:05:13 -0800

    FAQ and Sitemap

--- faq/index.php
+++ faq/index.php
@@ -1,7 +1,71 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Angela
- * Date: 11/3/2014
- * Time: 9:54 PM
- */ 
+<!doctype html>
+
+<html lang="en">
+<head>
+    <title>Emblem Objects - FAQ</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+    <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="../css/body.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+    <link rel="stylesheet" type="text/css" href="../css/nav.css">
+    <link rel="stylesheet" type="text/css" href="../css/content.css">
+    <link rel="stylesheet" type="text/css" href="../css/how-to.css">
+    <link rel="stylesheet" type="text/css" href="../css/footer.css">
+
+
+</head>
+
+
+<body>
+    <div id="wrapper">
+<?php include "../templates/header.php";?>
+
+
+        <!-- CONTENT -->
+        <div id="content">
+            <div class="container">
+              <p>&nbsp</p>
+              <center><h1>FAQ</h1></center>
+<h3>Q: What are you?</h3>
+A: We are EmblemObjects, a 3D printing design service, creating product tailored to your needs.
+<h3>Q: What is a Base Object?</h3>
+A: Base Objects are products that have been created for the purpose of having your designs 3D printed on them.
+<h3>Q: What is an Unique Object?</h3>
+A: Unique Objects are products that are fully realized by a designer, so what you see is what you get.
+<h3>Q: Who are designers?</h3>
+A: Designers create products you see on the site. They help you realize your images and designs on base objects of your choosing.
+<h3>Q: How do you 3D print?</h3>
+A: We use a number of commercial 3D printing services. This ensures that we give you the best objects with the most value.
+<h3>Q: What happens to my Design Request?</h3>
+A: Our designers gets your Design Requests and create object previews for you. These are real people, working very hard to get you the best results possible, please treat them with respect.
+<h3>Q: How do I pay for my order?</h3>
+A: We use PayPal checkout. They are one of the most trusted name in online payments and have excellent customer service.
+<h3>Q: Can I use my credit card for payment, if I don’t have a PayPal account?</h3>
+A: Yes you can. We use PayPal Guest Checkout so you don’t have to to be registered on PayPal.
+<h3>Q: Am I obligated to buy if I submit a Design Request?</h3>
+A: Of course not, if the object preview is not what you want, you don’t have to buy it. Just be considerate to our designers time. Thank you.
+<h3>Q: How long does it take for me to get my objects?</h3>
+A: It depends on the type objects and quantity of the order. Naturally, unique objects will take less because we can initiate the printing process right away. If you are designing a base object, it will take a bit longer since our designer have to create it then EmblemObjects perform a design
+review to approve it for quality. Usually, the entire process takes about a month. Thank you for your patience, We are working very hard on shortening this time.
+<h3>Q: What happens if my object does not pass design review?</h3>
+A: We will let you know that the object cannot be printed. There is no cost to you. Though this happens very rarely.
+<h3>Q: What if your 3D printers cannot print the object?</h3>
+A: We will let you that our printers cannot print the object, and release the authorization hold on your payment. You will not be charged until our printers confirm your object can be printed.
+<h3>Q: Do you take other currencies?</h3>
+A: At this time, we only on accept U.S. Dollars. We are always thinking of way to widen our market so check back often.
+<h3>Q: What is your shipping policy?</h3>
+A: We charge a flat rate $7 to shipping anywhere in US (except Hawaii and Alaska). At this point, we do not ship outside of the US.
+<p>&nbsp</p>
+            </div>
+        </div>
+        <div style="clear:both"></div>
+
+
+
+
+        <?php include "../templates/footer.php";?>
+    </div>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+</html>
--- sitemap/index.php
+++ sitemap/index.php
@@ -1,7 +1,57 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Angela
- * Date: 11/3/2014
- * Time: 9:54 PM
- */ 
+include_once '../../php/config.php';
+?>
+<!doctype html>
+
+
+<html lang="en">
+<head>
+    <title>Emblem Objects</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+    <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/header.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/nav.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/body.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/content.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/footer.css">
+    <style type="text/css">
+      .tab { margin-left: 20px;
+          text-decoration: none}
+    </style>
+    <style type="text/css">
+      .no-line {
+          text-decoration: none}
+    </style>
+</head>
+
+
+<body>
+
+    <center>
+      <table border="0" cellpadding="0" cellspacing="0" style="width: 60%; height: auto">
+        <tbody>
+        <tr>
+            <td>
+                <a href="../home/" class="no-line" style="font-size:20px">Home</a><br>
+                <a href="../whats-new/" class="no-line" style="font-size:20px" >What's New</a><br>
+                <a href="../how-to/" class="no-line" style="font-size:20px" >How To</a><br>
+                <a href="../shop" class="no-line" style="font-size:20px">Shop</a><br>
+                  <a href="../shop" class="tab">Accessories</a><br>
+                  <a href="../shop" class="tab">Art</a><br>
+                  <a href="../shop" class="tab">Gadgets</a><br>
+                  <a href="../shop" class="tab">Home</a><br>
+                  <a href="../shop" class="tab">Jewelry</a><br>
+                  <a href="../shop" class="tab">Novelty</a><br>
+                <a href="../shop" class="no-line" style="font-size:20px">Collections</a><br>
+                <a href="../shop" class="no-line" style="font-size:20px">Designer</a><br>
+                <a href="../shop" class="no-line" style="font-size:20px">Newest</a><br>
+                <a href="../shop" class="no-line" style="font-size:20px">Popular</a><br>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+  </center>
+
+</body>
+</html>
