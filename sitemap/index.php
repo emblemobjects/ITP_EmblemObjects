8905ee8924768f0d102c18a198c58df41ad407a7
Jose Valderruten <Josecamilo@me.com>
2014-11-06 18:46:09 -0800

    Sitemap

--- sitemap/index.php
+++ sitemap/index.php
@@ -1,68 +1,107 @@
 
-<?php
-include_once '../../php/config.php';
-?>
-<!doctype html>
+<?php include_once '../../php/config.php';?>
+<!-- doctype html -->
 
 <html lang="en">
 <head>
-    <title>Emblem Objects</title>
+    <title>Emblem Objects - Site Map</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
+    <script type="text/javascript" src="<?php echo $jQuery; ?>"></script>
+    <script type="text/javascript" src="<?php echo DIR; ?>/js/handlebars-v2.0.0.js"></script>
+    <script type="text/javascript" src="<?php echo DIR; ?>/js/facebook-js-sdk/facebook-sdk.js"></script>
+    <script type="text/javascript" src="<?php echo DIR; ?>/js/facebook-js-sdk/facebook-api.js"></script>
+    <script type="text/javascript" src="<?php echo DIR; ?>/js/config.js"></script>
+    <script type="text/javascript" src="<?php echo DIR; ?>/js/nav.js"></script>
+
     <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/header.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/nav.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/body.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/content.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $Dir;?>/css/footer.css">
+    <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,200' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="../css/core.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+    <link rel="stylesheet" type="text/css" href="../css/nav.css">
+    <link rel="stylesheet" type="text/css" href="../css/content.css">
+    <link rel="stylesheet" type="text/css" href="../css/footer.css">
+    <link rel="stylesheet" type="text/css" href="../css/home.css">
+    <link rel="stylesheet" type="text/css" href="../css/items-grid.css">
+    <link rel="stylesheet" type="text/css" href="../css/overlay.css">
+    <link rel="stylesheet" type="text/css" href="../css/item-info.css">
+
+<!-- STYLES -->
     <style type="text/css">
-      .tab { margin-left: 20px;
-          text-decoration: none}
-    </style>
-    <style type="text/css">
-      .no-line {
-          text-decoration: none}
+      .tab {
+        margin-left: 15px;
+        text-decoration: none;}
     </style>
 
+    <style type="text/css">
+      .tab2 {
+        margin-left: 30px;
+        text-decoration: none;}
+    </style>
+
+    <style type="text/css">
+      .main_link {
+        font-size:15px;
+        text-decoration: none;}
+    </style>
 </head>
 
-
-<body>
-
-    <center>
-      <table border="0" cellpadding="0" cellspacing="0" style="width: 60%; height: auto">
-        <tbody>
-        <tr>
-          <td>
-              <a href="../home/" class="no-line" style="font-size:20px">Home</a><br>
-              <a href="../whats-new/" class="no-line" style="font-size:20px" >What's New</a><br>
-              <a href="../how-to/" class="no-line" style="font-size:20px" >How To</a><br>
-              <a href="../shop" class="no-line" style="font-size:20px">Shop</a><br>
-                <a href="../shop" class="tab">Accessories</a><br>
-                <a href="../shop" class="tab">Art</a><br>
-                <a href="../shop" class="tab">Gadgets</a><br>
-                <a href="../shop" class="tab">Home</a><br>
-                <a href="../shop" class="tab">Jewelry</a><br>
-                <a href="../shop" class="tab">Novelty</a><br>
-              <a href="../shop" class="no-line" style="font-size:20px">Collections</a><br>
-              <a href="../shop" class="no-line" style="font-size:20px">Designer</a><br>
-              <a href="../shop" class="no-line" style="font-size:20px">Newest</a><br>
-              <a href="../shop" class="no-line" style="font-size:20px">Popular</a><br>
-          </td>
-        </tr>
-        </tbody>
-    </table>
-  </center>
-
-</body>
-
-        </div>
+  <body>
+    <?php include "../templates/header.php"; ?>
+    <?php include '../templates/nav.php'; ?>
+    <div style="margin-top: 50px ; width: 100%">
+        <a><center><h2> Site Map </h2></center></a><br>
+      <div style="float:left ; margin-left: 250px">
+          <a href="../whats-new/" class="main_link">What's New</a><br>
+          <a href="../how-to/" class="main_link">How To</a><br>
+          <a href="../shop" class="main_link">Collections</a><br>
+          <a href="../shop" class="main_link">Designer</a><br>
+          <a href="../shop" class="main_link">Newest</a><br>
+          <a href="../shop" class="main_link">Popular</a><br>
+      </div>
+      <div style="float:left ; margin-left: 50px;">
+          <a href="../shop" class="main_link">Shop</a><br>
+            <a href="../shop" class="tab">Accessories</a><br>
+              <a href="../shop" class="tab2">Cases</a><br>
+              <a href="../shop" class="tab2">Keychains</a><br>
+              <a href="../shop" class="tab2">Belts & Buckels</a><br>
+            <a href="../shop" class="tab">Art</a><br>
+              <a href="../shop" class="tab2">Sculpture</a><br>
+              <a href="../shop" class="tab2">Parametics</a><br>
+              <a href="../shop" class="tab2">Themed</a><br>
+      </div>
+      <div style="float:left ; margin-left: 50px; margin-top: 18px;">
+            <a href="../shop" class="tab">Gadgets</a><br>
+              <a href="../shop" class="tab2">Parts</a><br>
+              <a href="../shop" class="tab2">Props</a><br>
+            <a href="../shop" class="tab">Home</a><br>
+              <a href="../shop" class="tab2">Placements</a><br>
+              <a href="../shop" class="tab2">Holders</a><br>
+              <a href="../shop" class="tab2">Vases</a><br>
+              <a href="../shop" class="tab2">Lighting</a><br>
+              <a href="../shop" class="tab2">Desktop</a><br>
+      </div>
+      <div style="float:left ; margin-left: 50px; margin-top: 18px;">
+            <a href="../shop" class="tab">Jewelry</a><br>
+              <a href="../shop" class="tab2">Rings</a><br>
+              <a href="../shop" class="tab2">Pendants</a><br>
+              <a href="../shop" class="tab2">Necklaces</a><br>
+              <a href="../shop" class="tab2">Bracelets</a><br>
+              <a href="../shop" class="tab2">Earrings</a><br>
+              <a href="../shop" class="tab2">Cufflings</a><br>
+              <a href="../shop" class="tab2">Watches</a><br>
+            <a href="../shop" class="tab">Novelty</a><br>
+              <a href="../shop" class="tab2">Desk Toys</a><br>
+              <a href="../shop" class="tab2">Puzzles</a><br>
+      </div>
+      </div>
     </div>
-    <div style="clear:both"></div>
+  </body>
 
-
-    <div id="footer"></div>
+    <?php include "../templates/footer.php";?>
     <div style="clear:both"></div>
-</div>
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <div style="clear:both"></div>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 </html>
