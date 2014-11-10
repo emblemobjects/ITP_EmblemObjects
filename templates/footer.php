87e92c1be3d10f09a76af1cd97df241cac0c4520
tessvwu <veronitw@usc.edu>
2014-11-09 19:09:15 -0800

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,9 @@
    <!-- FOOTER -->
    <div id="footer">
     <div>
-        <a href="#twitter"><img height="35px" width="35px" src="<?php echo $Dir;?>/images/icon_twitter.png"/></a>
-        <a href="#"><img height="35px" width="35px" src="<?php echo $Dir;?>/images/icon_tumblr.png"/></a>
-        <a href="#"><img height="35px" width="35px" src="<?php echo $Dir;?>/images/icon_youtube.png"/></a>
-        <a href="#"><img height="35px" width="35px" src="<?php echo $Dir;?>/images/icon_google+.png"/></a>
-        <a href="#facebook"><img height="35px" width="35px" src="<?php echo $Dir;?>/images/icon_facebook.png"/></a>
+        <a href="#facebook"><div id="facebook"></div></a>
+        <a href="#twitter"><div id="twitter"></div></a>
+        <a href="#pinterest"><div id="pinterest"></div></a>
     </div>
    </div>
 <div style="clear:both"></div>
