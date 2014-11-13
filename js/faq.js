a13b60e5bc14e9b0dba34fc678fec91cb56e448f
Tyler Anne Isaman <isaman@usc.edu>
2014-11-13 12:11:16 -0800

    working with faq

---
+++ js/faq.js
@@ -0,0 +1,60 @@
+/**
+ * Created by TylerAnne on 11/13/14.
+ */
+
+
+$("#q1").click(function(){
+    $("#a1").toggle();
+});
+
+$("#q2").click(function(){
+    $("#a2").toggle();
+});
+
+$("#q3").click(function(){
+    $("#a3").toggle();
+});
+
+$("#q4").click(function(){
+    $("#a4").toggle();
+});
+
+$("#q5").click(function(){
+    $("#a5").toggle();
+});
+
+$("#q6").click(function(){
+    $("#a6").toggle();
+});
+
+$("#q7").click(function(){
+    $("#a7").toggle();
+});
+
+$("#q8").click(function(){
+    $("#a8").toggle();
+});
+
+$("#q9").click(function(){
+    $("#a9").toggle();
+});
+
+$("#q10").click(function(){
+    $("#a10").toggle();
+});
+
+$("#q11").click(function(){
+    $("#a11").toggle();
+});
+
+$("#q12").click(function(){
+    $("#a12").toggle();
+});
+
+$("#q13").click(function(){
+    $("#a13").toggle();
+});
+
+$("#q14").click(function(){
+    $("#a14").toggle();
+});
