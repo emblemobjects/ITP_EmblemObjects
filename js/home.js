61d9a638b5ca935836a2b31d5789c0284c6b7c17
Tyler Anne Isaman <isaman@usc.edu>
2014-11-13 16:59:34 -0800

    small changes

--- js/home.js
+++ js/home.js
@@ -130,12 +130,12 @@ store.priceUpdate = function(index) {
         
         for (var i = 0; i < store.items[id]['details'].length; i++){
             if (store.items[id]['details'][i]['size'] == size && store.items[id]['details'][i]['material_id']== material_id){
-                var newPrice = store.items[id]['details'][i]['price'];
+                var newSizePrice = store.items[id]['details'][i]['price'];
                 var detail_id = store.items[id]['details'][i]['detail_id'];
             }
         }
 
-        $("#price-update").html('Price: $' + newPrice);
+        $("#price-update").html('Price: $' + newSizePrice);
         $("input[name='detail_id']").val(detail_id);
 
     });
@@ -144,11 +144,11 @@ store.priceUpdate = function(index) {
         material_id = $(this).val();
         for (var i = 0; i < store.items[id]['details'].length; i++){
             if (store.items[id]['details'][i]['material_id']== material_id && store.items[id]['details'][i]['size'] == size){
-                var newPrice = store.items[id]['details'][i]['price'];
+                var newMatPrice = store.items[id]['details'][i]['price'];
                 var detail_id = store.items[id]['details'][i]['detail_id'];
             }
         }
-        $("#price-update").html('Price: $' + newPrice);
+        $("#price-update").html('Price: $' + newMatPrice);
         $("input[name='detail_id']").val(detail_id);
     });
 }
