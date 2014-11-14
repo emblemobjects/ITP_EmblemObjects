43aeaf641c0034d594cc61efc03580e5598453cc
Tyler Anne Isaman <isaman@usc.edu>
2014-11-13 17:04:22 -0800

    small changes

--- js/home.js
+++ js/home.js
@@ -1,32 +1,4 @@
 store = window.store || {};
-///////////////////////////////////////// HANDLEBARS HELPER
-
-// Handlebars.registerHelper('compare', function(lvalue, rvalue, options) {
-
-//     if (arguments.length < 3)
-//         throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
-
-//     operator = options.hash.operator || "==";
-
-//     var operators = {
-//         '===':      function(l,r) { return l === r; },
-//         '!=':       function(l,r) { return l != r; },
-//         '<':        function(l,r) { return l < r; },
-//         '>':        function(l,r) { return l > r; },
-//     }
-
-//     if (!operators[operator])
-//         throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
-
-//     var result = operators[operator](lvalue,rvalue);
-
-//     if( result ) {
-//         return options.fn(this);
-//     } else {
-//         return options.inverse(this);
-//     }
-
-// });
 
 // handlebars helper if equals
 Handlebars.registerHelper('ifEqual', function(value1, value2, options) {
